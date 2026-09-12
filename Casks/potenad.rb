ae73934a35235912035b5bedf6948ae43e452e14
Sathya Tadinada <sathyav.tadinada@gmail.com>
2026-09-12 16:00:40 -0600

    Add PoteNad cask

---
+++ Casks/potenad.rb
@@ -0,0 +1,19 @@
+cask "potenad" do
+  version "3.0.0"
+  sha256 "1c049e1636281155c6255376a858234e856395f90281a811b760bcec90449de8"
+
+  url "https://github.com/PoteNad/potenad/releases/download/v#{version}/PoteNad-#{version}-macOS.dmg"
+  name "PoteNad"
+  desc "Small native plain-text editor"
+  homepage "https://github.com/PoteNad/potenad"
+
+  depends_on macos: :ventura
+
+  app "PoteNad.app"
+
+  zap trash: [
+    "~/Library/Application Support/PoteNad",
+    "~/Library/Preferences/io.github.PoteNad.potenad.plist",
+    "~/Library/Saved Application State/io.github.PoteNad.potenad.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# PoteNad Homebrew Tap
+
+Install [PoteNad](https://github.com/PoteNad/potenad):
+
+```sh
+brew install --cask PoteNad/tap/potenad
+```
+
+PoteNad is not notarized. The first time you open it, macOS may ask you to allow it in **System Settings → Privacy & Security**.
